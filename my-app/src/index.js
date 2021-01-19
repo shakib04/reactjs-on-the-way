9d54c821a5379c292c78b28e19acc0c03d5f8c44
shakib <mdshakibulalam@gmail.com>
2021-01-19 21:26:07 +0600

    test added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-const name = "shakib vai";
+const name = "Shakibul Alam";
 const date = new Date();
 
 const headStyle = {
@@ -12,17 +12,18 @@ const headStyle = {
   fontSize: "50px"
 }
 
-
 ReactDOM.render(
   <div>
-    <h2 style={{color: 'red', fontSize: '75px', fontWeight: 'bold'}}>Welcome to React World</h2>
-    <h3 style={ headStyle }>Heading 3</h3>
-    <h1 className="name-style">{name}</h1>
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
-    quaerat natus quasi pariatur
-    quisquam deleniti aut
+    <h2 className="h2-design" style={{ color: 'white', fontSize: '75px', fontWeight: 'bold' }}>Welcome to React World</h2>
+    <div className="card">
+      <h3 style={headStyle}>Heading 3</h3>
+      <h1 className="name-style">{name}</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
+      quaerat natus quasi pariatur
+      quisquam deleniti aut
     iusto enim tempora repudiandae?</p>
-    <h2>{date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear()}</h2>
+      <h2>{date.getDate() + "-" + date.getUTCMonth() + "-" + date.getFullYear()}</h2>
+    </div>
   </div>,
   document.getElementById('root')
 );
